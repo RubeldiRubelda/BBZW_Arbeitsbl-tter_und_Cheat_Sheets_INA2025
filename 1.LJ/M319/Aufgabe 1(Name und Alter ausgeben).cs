401e27ec67e9207679ba5305a4a88ee0f3e31829
RubeldiRubelda <benjamin.kunz.ch@gmail.com>
2025-11-02 19:17:36 +0100

    Upload

---
+++ 1.LJ/M319/Aufgabe 1(Name und Alter ausgeben).cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Kap02_Aufgabe01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName; //Hier wird die String variable definiert
+            string lastName;
+            int ageRightNow;
+
+
+            Console.Write("Wie lautet dein Vorname? "); // Ausgabe der Frage
+            firstName = Console.ReadLine(); // Ermittlung des Vornahmens
+            // NICHT AUSFÜHRBARER CODE DENN ES IST EIN KOMMENTAER
+
+            Console.Write("Wie lautet dein Nachname? ");
+            lastName = Console.ReadLine();
+
+
+            Console.Write("Wie alt bist du?");
+            ageRightNow = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine("\n----------------------------------\n"); // Der Tolle Abstand
+            Console.WriteLine("Hallo " + firstName + " " + lastName + ". " + "Bald wirst du " + (ageRightNow + 1) + "."); // Ausgabe des Textes
+            Console.ReadLine(); //LEERSCHLAG
+
+        }
+    }
+} 
